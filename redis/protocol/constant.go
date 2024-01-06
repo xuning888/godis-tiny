573ced38f3dc86d935fef14c95d5141c5e4e1c14
xuning <xningdd@163.com>
2024-01-06 21:05:35 +0800

    resp协议解析

--- redis/protocol/constant.go
+++ redis/protocol/constant.go
@@ -1,8 +1,9 @@
 package protocol
 
 var (
-	NullBulkReplyBytes = []byte("$-1" + CRCF)
-	PongReplyBytes     = []byte("+PONG" + CRCF)
+	NullBulkReplyBytes  = []byte("$-1" + CRLF)
+	emptyMultiBulkBytes = []byte("*0" + CRLF)
+	PongReplyBytes      = []byte("+PONG" + CRLF)
 )
 
 type PongReply struct {
@@ -10,4 +11,29 @@ type PongReply struct {
 
 func (p *PongReply) ToBytes() []byte {
 	return PongReplyBytes
+}
+
+func MakePongReply() *PongReply {
+	return &PongReply{}
+}
+
+type NullBulkReply struct {
+}
+
+func (n *NullBulkReply) ToBytes() []byte {
+	return NullBulkReplyBytes
+}
+
+func MakeNullBulkReply() *NullBulkReply {
+	return &NullBulkReply{}
+}
+
+type EmptyMultiBulkReply struct{}
+
+func (r *EmptyMultiBulkReply) ToBytes() []byte {
+	return emptyMultiBulkBytes
+}
+
+func MakeEmptyMultiBulkReply() *EmptyMultiBulkReply {
+	return &EmptyMultiBulkReply{}
 }
--- redis/protocol/type.go
+++ redis/protocol/type.go
@@ -3,10 +3,11 @@ package protocol
 import (
 	"bytes"
 	"fmt"
+	"g-redis/interface/redis"
 )
 
 var (
-	CRCF = "\r\n"
+	CRLF = "\r\n"
 )
 
 // ErrReply 错误信息
@@ -36,7 +37,7 @@ type IntReply struct {
 }
 
 func (r *IntReply) ToBytes() []byte {
-	return []byte(fmt.Sprintf(":%d%s", r.num, CRCF))
+	return []byte(fmt.Sprintf(":%d%s", r.num, CRLF))
 }
 
 func MakeIntReply(num int64) *IntReply {
@@ -54,7 +55,7 @@ func (b *BulkReply) ToBytes() []byte {
 	if b.Arg == nil {
 		return NullBulkReplyBytes
 	}
-	return []byte(fmt.Sprintf("$%d%s%s%s", len(b.Arg), CRCF, string(b.Arg), CRCF))
+	return []byte(fmt.Sprintf("$%d%s%s%s", len(b.Arg), CRLF, string(b.Arg), CRLF))
 }
 
 func MakeBulkReply(arg []byte) *BulkReply {
@@ -65,24 +66,61 @@ func MakeBulkReply(arg []byte) *BulkReply {
 
 // MultiBulkReply 数组返回
 type MultiBulkReply struct {
-	Rows [][]byte
+	Args [][]byte
 }
 
-func MakeMultiBulkReply(rows [][]byte) *MultiBulkReply {
+func MakeMultiBulkReply(args [][]byte) *MultiBulkReply {
 	return &MultiBulkReply{
-		Rows: rows,
+		Args: args,
 	}
 }
 
 func (m *MultiBulkReply) ToBytes() []byte {
-	if m.Rows == nil {
+	if m.Args == nil {
 		return NullBulkReplyBytes
 	}
-	numRows := len(m.Rows)
+	argLen := len(m.Args)
 	var buf bytes.Buffer
-	buf.WriteString(fmt.Sprintf("*%d%s", numRows, CRCF))
-	for _, row := range m.Rows {
+	buf.WriteString(fmt.Sprintf("*%d%s", argLen, CRLF))
+	for _, row := range m.Args {
 		buf.Write(MakeBulkReply(row).ToBytes())
 	}
 	return buf.Bytes()
+}
+
+type MultiRowReply struct {
+	replies []redis.Reply
+}
+
+func MakeMultiRowReply(replies []redis.Reply) *MultiRowReply {
+	return &MultiRowReply{
+		replies: replies,
+	}
+}
+
+func (m *MultiRowReply) ToBytes() []byte {
+	if m.replies == nil {
+		return NullBulkReplyBytes
+	}
+	argLen := len(m.replies)
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("*%d%s", argLen, CRLF))
+	for _, reply := range m.replies {
+		buf.Write(reply.ToBytes())
+	}
+	return buf.Bytes()
+}
+
+type StandardErrReply struct {
+	Status string
+}
+
+func (s *StandardErrReply) ToBytes() []byte {
+	return []byte("-" + s.Status + CRLF)
+}
+
+func MakeStandardErrReply(status string) *StandardErrReply {
+	return &StandardErrReply{
+		Status: status,
+	}
 }
