ea500a30182194ca984d85901ea2e22342380540
xuning <xningdd@163.com>
2024-01-08 01:21:32 +0800

    使用rwlock太不划算了，不如遍历

--- datastruct/dict/sync_dict.go
+++ datastruct/dict/sync_dict.go
@@ -2,65 +2,45 @@ package dict
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 type SimpleSync struct {
-	data    sync.Map
-	count   int32
-	rwMutex sync.RWMutex
+	data sync.Map
 }
 
 func (s *SimpleSync) Get(key string) (value interface{}, exists bool) {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
 	load, ok := s.data.Load(key)
 	return load, ok
 }
 
 func (s *SimpleSync) Len() int {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
-	return int(atomic.LoadInt32(&s.count))
+	result := 0
+	s.data.Range(func(key, _ interface{}) bool {
+		result++
+		return true
+	})
+	return result
 }
 
 func (s *SimpleSync) Put(key string, value interface{}) (result int) {
-	s.rwMutex.Lock()
-	defer func() {
-		s.rwMutex.Unlock()
-	}()
 	_, ok := s.data.Load(key)
 	s.data.Store(key, value)
 	if ok {
 		return 0
 	}
-	s.addCount()
 	return 1
 }
 
 func (s *SimpleSync) PutIfAbsent(key string, value interface{}) (result int) {
-	s.rwMutex.Lock()
-	defer func() {
-		s.rwMutex.Unlock()
-	}()
 	_, ok := s.data.Load(key)
 	if ok {
 		return 0
 	}
 	s.data.Store(key, value)
-	s.addCount()
 	return 1
 }
 
 func (s *SimpleSync) PutIfExists(key string, value interface{}) (result int) {
-	s.rwMutex.Lock()
-	defer func() {
-		s.rwMutex.Unlock()
-	}()
 	_, ok := s.data.Load(key)
 	if ok {
 		s.data.Store(key, value)
@@ -70,24 +50,15 @@ func (s *SimpleSync) PutIfExists(key string, value interface{}) (result int) {
 }
 
 func (s *SimpleSync) Remove(key string) (result int) {
-	s.rwMutex.Lock()
-	defer func() {
-		s.rwMutex.Unlock()
-	}()
 	_, ok := s.data.Load(key)
 	if ok {
 		s.data.Delete(key)
-		s.desCount()
 		return 1
 	}
 	return 0
 }
 
 func (s *SimpleSync) ForEach(consumer Consumer) {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
 	s.data.Range(func(key, value interface{}) bool {
 		entity := key.(string)
 		return consumer(entity, value)
@@ -95,10 +66,6 @@ func (s *SimpleSync) ForEach(consumer Consumer) {
 }
 
 func (s *SimpleSync) Keys() []string {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
 	result := make([]string, s.Len())
 	i := 0
 	s.data.Range(func(key, _ interface{}) bool {
@@ -110,10 +77,6 @@ func (s *SimpleSync) Keys() []string {
 }
 
 func (s *SimpleSync) RandomKeys(limit int) []string {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
 	if limit >= s.Len() {
 		limit = s.Len()
 	}
@@ -128,10 +91,6 @@ func (s *SimpleSync) RandomKeys(limit int) []string {
 }
 
 func (s *SimpleSync) RandomDistinctKeys(limit int) []string {
-	s.rwMutex.RLock()
-	defer func() {
-		s.rwMutex.RUnlock()
-	}()
 	if limit >= s.Len() {
 		limit = s.Len()
 	}
@@ -149,14 +108,6 @@ func (s *SimpleSync) RandomDistinctKeys(limit int) []string {
 
 func (s *SimpleSync) Clear() {
 	*s = *MakeSimpleSync()
-}
-
-func (s *SimpleSync) addCount() int32 {
-	return atomic.AddInt32(&s.count, 1)
-}
-
-func (s *SimpleSync) desCount() int32 {
-	return atomic.AddInt32(&s.count, -1)
 }
 
 func MakeSimpleSync() *SimpleSync {
