6d681e784070a2ab6817e788f9a7d663a511d83d
xuning <xningdd@163.com>
2024-01-09 16:02:59 +0800

    删除非必要代码

--- database/database.go
+++ database/database.go
@@ -3,14 +3,10 @@ package database
 import (
 	"fmt"
 	"g-redis/datastruct/dict"
-	"g-redis/datastruct/lock"
 	"g-redis/interface/database"
 	"g-redis/interface/redis"
-	"g-redis/pkg/timewheel"
 	"g-redis/redis/protocol"
-	"log"
 	"strings"
-	"time"
 )
 
 // cmdLint database 内部流转的结构体，包含了客户端发送的命令名称和数据
@@ -51,40 +47,28 @@ type ExeFunc func(db *DB, cmdLint *cmdLint) redis.Reply
 
 // DB 存储
 type DB struct {
-	index      int
-	data       dict.Dict
-	ttlMap     dict.Dict
-	versionMap dict.Dict
-	locker     *lock.Locks
+	index int
+	data  dict.Dict
 }
 
 func MakeDB(index int) *DB {
 	return &DB{
-		index:      index,
-		data:       dict.MakeConcurrent(16),
-		ttlMap:     dict.MakeConcurrent(16),
-		versionMap: dict.MakeConcurrent(16),
-		locker:     lock.Make(1),
+		index: index,
+		data:  dict.MakeConcurrent(16),
 	}
 }
 
 func MakeSimpleDb(index int) *DB {
 	return &DB{
-		index:      index,
-		data:       dict.MakeSimpleDict(),
-		ttlMap:     dict.MakeSimpleDict(),
-		versionMap: dict.MakeSimpleDict(),
-		locker:     lock.Make(1),
+		index: index,
+		data:  dict.MakeSimpleDict(),
 	}
 }
 
 func MakeSimpleSync(index int) *DB {
 	return &DB{
-		index:      index,
-		data:       dict.MakeSimpleSync(),
-		ttlMap:     dict.MakeSimpleSync(),
-		versionMap: dict.MakeSimpleSync(),
-		locker:     lock.Make(1),
+		index: index,
+		data:  dict.MakeSimpleSync(),
 	}
 }
 
@@ -122,9 +106,6 @@ func (db *DB) GetEntity(key string) (*database.DataEntity, bool) {
 	if !exists {
 		return nil, false
 	}
-	if db.IsExpired(key) {
-		return nil, false
-	}
 	entity, _ := row.(*database.DataEntity)
 	return entity, true
 }
@@ -144,9 +125,6 @@ func (db *DB) PutIfAbsent(key string, entity *database.DataEntity) int {
 // Remove 删除数据
 func (db *DB) Remove(key string) {
 	db.data.Remove(key)
-	db.ttlMap.Remove(key)
-	taskKey := genExpireTask(key)
-	timewheel.Cancel(taskKey)
 }
 
 func (db *DB) Removes(keys ...string) (deleted int) {
@@ -176,98 +154,4 @@ func (db *DB) Exists(keys []string) int64 {
 
 func (db *DB) Flush() {
 	db.data.Clear()
-	db.ttlMap.Clear()
-}
-
-/* ---- lock function ----- */
-
-func (db *DB) RWLocks(writeKeys []string, readKeys []string) {
-	db.locker.RWLocks(writeKeys, readKeys)
-}
-
-// RWUnLocks unlock keys for writing and reading
-func (db *DB) RWUnLocks(writeKeys []string, readKeys []string) {
-	db.locker.RWUnLocks(writeKeys, readKeys)
-}
-
-/* ---- Data TTL ----- */
-
-func genExpireTask(key string) string {
-	return "expire:" + key
-}
-
-// Expire sets ttlCmd of key
-func (db *DB) Expire(key string, expireTime time.Time) {
-	db.ttlMap.Put(key, expireTime)
-	taskKey := genExpireTask(key)
-	timewheel.At(expireTime, taskKey, func() {
-		keys := []string{key}
-		db.RWLocks(keys, nil)
-		defer db.RWUnLocks(keys, nil)
-		// check-lock-check, ttl may be updated during waiting lock
-		log.Println("expire " + key)
-		rawExpireTime, ok := db.ttlMap.Get(key)
-		if !ok {
-			return
-		}
-		expireTime, _ := rawExpireTime.(time.Time)
-		expired := time.Now().After(expireTime)
-		if expired {
-			db.Remove(key)
-		}
-	})
-}
-
-// Persist cancel ttlCmd of key
-func (db *DB) Persist(key string) {
-	db.ttlMap.Remove(key)
-	taskKey := genExpireTask(key)
-	timewheel.Cancel(taskKey)
-}
-
-// IsExpired check whether a key is expired
-func (db *DB) IsExpired(key string) bool {
-	rawExpireTime, ok := db.ttlMap.Get(key)
-	if !ok {
-		return false
-	}
-	expireTime, _ := rawExpireTime.(time.Time)
-	expired := time.Now().After(expireTime)
-	if expired {
-		db.Remove(key)
-	}
-	return expired
-}
-
-/* --- add version --- */
-
-func (db *DB) addVersion(keys ...string) {
-	for _, key := range keys {
-		versionCode := db.GetVersion(key)
-		db.versionMap.Put(key, versionCode+1)
-	}
-}
-
-// GetVersion returns version code for given key
-func (db *DB) GetVersion(key string) uint32 {
-	entity, ok := db.versionMap.Get(key)
-	if !ok {
-		return 0
-	}
-	return entity.(uint32)
-}
-
-// ForEach traverses all the keys in the database
-func (db *DB) ForEach(cb func(key string, data *database.DataEntity, expiration *time.Time) bool) {
-	db.data.ForEach(func(key string, raw interface{}) bool {
-		entity, _ := raw.(*database.DataEntity)
-		var expiration *time.Time
-		rawExpireTime, ok := db.ttlMap.Get(key)
-		if ok {
-			expireTime, _ := rawExpireTime.(time.Time)
-			expiration = &expireTime
-		}
-
-		return cb(key, entity, expiration)
-	})
 }
